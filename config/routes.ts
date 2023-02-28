642b509b9369d6b70e587ac4ed637fbecce76e3a
machao07 <machao_07@163.com>
2023-02-28 09:23:46 +0800

    删除多余页面

--- config/routes.ts
+++ config/routes.ts
@@ -11,53 +11,31 @@
  * @doc https://umijs.org/docs/guides/routes
  */
 export default [
-  {
-    path: '/user',
-    layout: false,
-    routes: [
-      {
-        name: 'login',
-        path: '/user/login',
-        component: './User/Login',
-      },
-    ],
-  },
-  {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: '/admin',
-        redirect: '/admin/sub-page',
-      },
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        component: './Admin',
-      },
-    ],
-  },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-  },
-  {
-    path: '/',
-    redirect: '/welcome',
-  },
-  {
-    path: '*',
-    layout: false,
-    component: './404',
-  },
+    {
+        path: '/user',
+        layout: false,
+        routes: [
+            {
+                name: 'login',
+                path: '/user/login',
+                component: './User/Login',
+            },
+        ],
+    },
+    {
+        path: '/welcome',
+        name: 'welcome',
+        access: 'normal',
+        icon: 'smile',
+        component: './Welcome',
+    },
+    {
+        path: '/',
+        redirect: '/welcome',
+    },
+    {
+        path: '*',
+        layout: false,
+        component: './404',
+    },
 ];
