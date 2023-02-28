0d97b5ca6b754c65192ff0ccda8c66d336102f6e
machao07 <machao_07@163.com>
2023-02-28 09:25:36 +0800

    添加权限

--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,8 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
-export default function access(initialState: { currentUser?: AppModels.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
-  return {
-    canAdmin: currentUser && currentUser.roles?.includes('admin'),
-  };
+export default function access() {
+    return {
+        normal: true,
+    };
 }
